2ef558099494aab6170584f4106a2a0e1148f861
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-06-02 18:51:35 -0600

    generación de modulos y servicios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MachinesModule } from './machines/machines.module';
+import { RentalsModule } from './rentals/rentals.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MachinesModule, RentalsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
